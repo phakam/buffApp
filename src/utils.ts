f64f41da06aeca79174b0fb4016b105cb187adaf
Mpho Phaka <mpho@plusplusminus.co.za>
2023-02-26 14:18:24 +0200

    Add try catch on async calls

--- src/utils.ts
+++ src/utils.ts
@@ -5,70 +5,84 @@ export const setTableData = async (
   obj: {
     name: string;
     surname: string;
-  },
+  }
 ) => {
   const jsonValue = JSON.stringify(obj);
   if (name && jsonValue) {
-    await AsyncStorage.setItem(name, jsonValue);
+    try {
+      await AsyncStorage.setItem(name, jsonValue);
+    } catch (error) {
+      console.log('Error storage table data');
+    }
   }
 };
 
 export const getNameTable = async () => {
-  const token = await AsyncStorage.getItem('nameTable');
-
-  return token ? JSON.parse(token) : {};
+  try {
+    const data = await AsyncStorage.getItem('nameTable');
+    return data;
+  } catch (error) {
+    console.log('Error getting name table data');
+    return {};
+  }
 };
 
 export const getContactTable = async () => {
-  const token = await AsyncStorage.getItem('contactTable');
-
-  return token ? JSON.parse(token) : {};
+  try {
+    const data = await AsyncStorage.getItem('contactTable');
+    return data;
+  } catch (error) {
+    console.log('Error getting contact table data');
+    return {};
+  }
 };
 
-export const getNameData = async () => {
-  const asyncNameData = await AsyncStorage.getItem('nameTable');
-
-  if (!asyncNameData) {
-    const nameTableObject = {
-      '1': {name: 'Michael', surname: 'Baker'},
-    };
-
-    const jsonValue = JSON.stringify(nameTableObject);
-    await AsyncStorage.setItem('nameTable', jsonValue);
-  }
-
-}
-
 export const checkAndSetNameData = async () => {
-  const asyncNameData = await AsyncStorage.getItem('nameTable');
+  try {
+    const asyncNameData = await AsyncStorage.getItem('nameTable');
 
-  if (!asyncNameData) {
-    const nameTableObject = {
-      '1': {name: 'Michael', surname: 'Baker'},
-    };
+    if (!asyncNameData) {
+      const nameTableObject = {
+        '1': { name: 'Michael', surname: 'Baker' }
+      };
 
-    const jsonValue = JSON.stringify(nameTableObject);
-    await AsyncStorage.setItem('nameTable', jsonValue);
+      const jsonValue = JSON.stringify(nameTableObject);
+      await AsyncStorage.setItem('nameTable', jsonValue);
+    }
+  } catch (error) {
+    console.log('Error getting and setting name table data');
   }
-}
+};
 
 export const checkAndSetContactData = async () => {
-  const asyncContactData = await AsyncStorage.getItem('contactTable');
+  try {
+    const asyncContactData = await AsyncStorage.getItem('contactTable');
 
-  if (!asyncContactData) {
-    const contactTableObject = {
-      '1': {email: 'michael@test.com', cell_no: '0825558364'},
-    };
+    if (!asyncContactData) {
+      const contactTableObject = {
+        '1': { email: 'michael@test.com', cell_no: '0825558364' }
+      };
 
-    const jsonValue = JSON.stringify(contactTableObject);
-    await AsyncStorage.setItem('contactTable', jsonValue);
+      const jsonValue = JSON.stringify(contactTableObject);
+      await AsyncStorage.setItem('contactTable', jsonValue);
+    }
+  } catch (error) {
+    console.log('Error getting and setting contact table data');
   }
-}
+};
 
 export const clearContactData = async () => {
-  await AsyncStorage.removeItem('contactTable')
+  try {
+    await AsyncStorage.removeItem('contactTable');
+  } catch (error) {
+    console.log('Error clearing contact table data');
+  }
 };
 
 export const clearNameData = async () => {
-  await AsyncStorage.removeItem('nameTable')
+  try {
+    await AsyncStorage.removeItem('nameTable');
+  } catch (error) {
+    console.log('Error clearing name table data');
+  }
 };
